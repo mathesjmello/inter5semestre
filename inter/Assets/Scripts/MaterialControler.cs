b337838c376b3add91ba4440ef3d5a652bb14c5a
Matheus <matheus.mello@vetorzero.com.br>
2018-09-05 18:24:25 -0300

    tentando fazer a agua funcionar

---
+++ inter/Assets/Scripts/MaterialControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Substance.Game;
+
+
+public class MaterialControler : MonoBehaviour
+{
+
+	public SubstanceGraph WaterSubstance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnSliderChange(float value)
+	{
+		WaterSubstance.SetInputFloat("random",value);
+	}
+}

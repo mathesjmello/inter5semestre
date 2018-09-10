6e72efa9459de63bfb1e47bec35b7ceb98af0f54
Matheus <matheus.mello@vetorzero.com.br>
2018-09-10 19:52:01 -0300

    comecando com os mini games

---
+++ inter/Assets/Scripts/MiniGames/SetDifficult.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetDifficult : MonoBehaviour
+{
+	public Image BackGround;
+	public List<Sprite> PlayableBackground;
+	private Sprite _choosedSprite;
+	public List<Sprite> EasyGame;
+	public List<Sprite> MedioGame;
+	public List<Sprite> HardGame;
+	// Use this for initialization
+	void Start () {
+		if (PlayableBackground==null)
+		{
+			var i = Random.Range(0, 2);
+			GetDifficult(i);
+		}
+		PickBackground(PlayableBackground);
+		BackGround.sprite = _choosedSprite;
+	}
+
+	public void GetDifficult(int n)
+	{
+		switch (n)
+		{
+			case 0:
+				PlayableBackground = EasyGame;
+				break;
+			case 1:
+				PlayableBackground = MedioGame;
+				break;
+			case 2:
+				PlayableBackground = HardGame;
+				break;
+		}
+	}
+
+	void PickBackground(List<Sprite> difficult)
+	{
+		var sprites = difficult.ToArray();
+		var i = Random.Range(0, sprites.Length);
+		_choosedSprite = sprites[i];
+	}
+}
